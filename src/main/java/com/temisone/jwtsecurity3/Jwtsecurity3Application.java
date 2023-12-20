b2f2a95b0b98c56c5cf7e74787bf07e39e090e4a
jhjang7408 <jhjang7408@naver.com>
2023-12-20 15:53:28 +0900

    jwtsecurity 만들기 위한 세팅

---
+++ src/main/java/com/temisone/jwtsecurity3/Jwtsecurity3Application.java
@@ -0,0 +1,13 @@
+package com.temisone.jwtsecurity3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jwtsecurity3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jwtsecurity3Application.class, args);
+    }
+
+}
